2c7e78e4c95c2cde1266627dad3c673eb0a7c655
KkarolaS <133031923+KkarolaS@users.noreply.github.com>
2023-11-08 08:11:37 +0100

    create functionality for notes and add some styling to main page

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+let notes = [];
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/view", (req, res) => {
+  res.render("view.ejs", { notes: notes });
+});
+
+app.post("/submit", (req, res) => {
+  const title = req.body["title"];
+  const note = req.body["note"];
+  notes.push({ title: title, note: note, isEdited: false });
+  res.render("index.ejs", { title: title, note: note });
+});
+
+app.post("/delete", (req, res) => {
+  const indexArr = Object.keys(req.body);
+  const noteIndex = Number(indexArr[0]);
+  const newNotes = notes.filter((item, index) => index !== noteIndex);
+  notes = newNotes;
+  res.render("view.ejs", { notes: notes });
+});
+
+app.post("/edit", (req, res) => {
+  const indexArr = Object.keys(req.body);
+  const noteIndex = Number(indexArr[0]);
+  const newNotes = notes.map((item, index) => {
+    if (index === noteIndex) {
+      item.isEdited = true;
+    }
+    return item;
+  });
+  notes = newNotes;
+  res.render("view.ejs", { notes: notes });
+});
+
+app.post("/save", (req, res) => {
+  const changedTitle = req.body["changedTitle"];
+  const changedNote = req.body["changedNote"];
+  const indexArr = Object.keys(req.body);
+  const noteIndex = Number(indexArr[0]);
+  notes.map((item, index) => {
+    if (index === noteIndex) {
+      item.title = changedTitle;
+      item.note = changedNote;
+      item.isEdited = false;
+    }
+    return item;
+  });
+  res.render("view.ejs", { notes: notes });
+});
+
+app.listen(3000, () => {
+  console.log(`Listening on ${port} port`);
+});
